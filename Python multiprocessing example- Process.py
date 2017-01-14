b891c98cfcefe54e211f5df20c3daa9f46579d30
Madhu G Nadig <madhug.nadig@gmail.com>
2017-01-14 21:02:43 +0530

    Add python multiprocessing process example

---
+++ Python multiprocessing example- Process.py
@@ -0,0 +1,35 @@
+import multiprocessing as mp
+import math
+import os
+
+#----------------------------------------------------------------------------------------------------------------------
+#This is a VERY SIMPLE example of parallel processing in Python using the multiprocessing library using the Process function.
+#----------------------------------------------------------------------------------------------------------------------
+
+
+def process_info():
+    print('Module:' + str(__name__) + '\n')
+    print('Parent Process id:' + str(os.getppid())+ '\n' )
+    print('Process id:' + str(os.getpid())+ '\n\n' )
+
+def cubes_and_sqare_root(a,x,output):
+	process_info()
+	output.put((int(x), math.sqrt(a**3)))
+
+def main():
+	#Using the queue as the message passing paradigm 
+	output = mp.Queue()
+	processes = [mp.Process(target=cubes_and_sqare_root, args=(x, x,output)) for x in range(1,8)]
+
+	for process in processes:
+		process.start()
+
+	for process in processes:
+		process.join()
+
+	results = [output.get() for process in processes]
+	
+	print(results)
+
+if __name__ == '__main__':    
+	main()
