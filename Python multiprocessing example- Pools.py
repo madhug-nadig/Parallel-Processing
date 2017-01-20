8c4dcfe179ebfa059042d5ecd0a112e613fc9d47
Madhu G Nadig <madhug.nadig@gmail.com>
2017-01-20 06:56:17 +0530

    Add timing to Pool

--- Python multiprocessing example- Pools.py
+++ Python multiprocessing example- Pools.py
@@ -1,6 +1,7 @@
 import multiprocessing as mp
 import math
 import os
+import time
 
 #----------------------------------------------------------------------------------------------------------------------
 #This is a VERY SIMPLE example of parallel processing in Python using the multiprocessing library using the Pools object.
@@ -13,13 +14,18 @@ def process_info():
     print('Process id:' + str(os.getpid())+ '\n\n' )
 
 def cubes_and_sqare_root(a):
-	process_info()
+	#process_info()
 	return (int(a), math.sqrt(a**3))
 
 def main():
 	pool = mp.Pool(processes= mp.cpu_count())
-	results = [ pool.map(cubes_and_sqare_root, (x for x in range(1,8))) ]
-	print(results)
+	
+	s = time.clock()
+	results = [ pool.map(cubes_and_sqare_root, (x for x in range(1,10000000))) ]
+	e = time.clock()
+	
+	#print(results)
+	print(e-s)
 
 if __name__ == '__main__':
 	main()
