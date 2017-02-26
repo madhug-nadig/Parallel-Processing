4a82f67364bc8b9c46d1bb898dc0d768bb7a0ae4
Madhu G Nadig <madhug.nadig@gmail.com>
2017-02-26 13:18:44 +0530

    Finish up K-NN in parallel

--- K Nearest Neighbours - In Parallel.py
+++ K Nearest Neighbours - In Parallel.py
@@ -4,16 +4,18 @@
 #----------------------------------------------------------------------------------------------------------------
 #================================================================================================================
 
+
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
-import multiprocessing as mp
 import pandas as pd
 import random
 from collections import Counter
 from sklearn import preprocessing
 from itertools import repeat
+import multiprocessing as mp
+import time
 
 
 #for plotting
@@ -25,14 +27,8 @@ class CustomKNN:
 		self.accurate_predictions = 0
 		self.total_predictions = 0
 		self.accuracy = 0.0
-		
-	def distances_for_parallel(self, features, incoming, group):
-		return (np.linalg.norm(np.array(features)- np.array(incoming)), group)
 
-	def predict(self, training_data, to_predict, group, k = 3):
-		#training_data = dict(item for item in training_data)  # Convert back to a dict
-		print(training_data)
-
+	def predict(self, training_data, to_predict, k = 3):
 		if len(training_data) >= k:
 			print("K cannot be smaller than the total voting groups(ie. number of training data points)")
 			return
@@ -47,23 +43,25 @@ class CustomKNN:
 		result = Counter(results).most_common(1)[0][0]
 		confidence = Counter(results).most_common(1)[0][1]/k
 		
-		if result == group:
-			self.accurate_predictions += 1
-		
-		else:
-			print("Wrong classification with confidence " + str(confidence * 100) + " and class " + str(result))
-		
-		self.total_predictions += 1
-
+		return result, to_predict
 	
-	def test(self, test_set, training_data):
+	def test(self, test_set, training_set):
 		pool = mp.Pool(processes= 8)
 
-		
+		arr = {}
+		s = time.clock()
 		for group in test_set:
-			training_data = list(training_data.items())
-			print(type(training_data))
-			pool.starmap(self.predict, zip(training_data, test_set[group], repeat(group), repeat(3)))
+			arr[group] =  pool.starmap(self.predict, zip(repeat(training_set), test_set[group], repeat(3)))
+		e = time.clock()
+
+		#Calculating Accuracy
+		for group in test_set:
+			for data in test_set[group]:
+				for i in arr[group]:
+					if data == i[1]:
+						self.total_predictions += 1
+						if group == i[0]:
+							self.accurate_predictions+=1
 		
 		self.accuracy = 100*(self.accurate_predictions/self.total_predictions)
 		print("\nAcurracy :", str(self.accuracy) + "%")
@@ -101,7 +99,7 @@ def main():
 	random.shuffle(dataset)
 
 	#20% of the available data will be used for testing
-	test_size = 0.2
+	test_size = 0.1
 
 	#The keys of the dict are the classes that the data is classfied into
 	training_set = {2: [], 4:[]}
@@ -118,9 +116,13 @@ def main():
 	#Insert data into the test set
 	for record in training_data:
 		test_set[record[-1]].append(record[:-1]) # Append the list in the dict will all the elements of the record except the class
-
+	
+	s = time.clock()
 	knn = CustomKNN()
 	knn.test(test_set, training_set)
-
+	e = time.clock()
+	
+	print("Exec Time: ", e-s)
+	
 if __name__ == "__main__":
 	main()
