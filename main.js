7153c2dfce995adfde662c58f744d519dc62c44f
Madhu G Nadig <madhug.nadig@gmail.com>
2017-04-15 07:43:20 +0530

    Update main thread

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 	function compute(){
 		if (window.Worker) { // Check if the Browser supports the Worker api.
 			// Requires script name as input
-			var worker = new Worker("task.js");
+			var worker = new Worker("three-hump-camel-function.js");
 
 			worker.postMessage([0.554,2]); // Sending message as an array to the worker
 			console.log('Message posted to worker');
@@ -13,4 +13,3 @@
 			};
 		}
 	}
-	
